139cc42023e09ba7908f5d8c76830e3e61f389e2
Luis B <luis.beroiza@adesso.de>
2023-08-28 23:23:17 +0200

    Repara test unitarios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {RouterOutlet} from "@angular/router";
+import {FooterComponent} from "./footer/footer.component";
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [AppComponent, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet]
   }));
 
   it('should create the app', () => {
--- src/app/clients/client.spec.ts
+++ src/app/clients/client.spec.ts
@@ -2,6 +2,13 @@ import { Client } from './client';
 
 describe('Client', () => {
   it('should create an instance', () => {
-    expect(new Client()).toBeTruthy();
+    const client: Client = new Client(
+      1,
+      'Name',
+      'surname',
+      'email@email.com',
+      '1983-10-06',
+      '2023-08-28');
+    expect(client).toBeTruthy();
   });
 });
