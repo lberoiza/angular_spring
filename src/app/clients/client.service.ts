518769a035f9f45391cd38896df999336cdcb069
Luis B <luis.beroiza@adesso.de>
2023-08-29 12:57:50 +0200

    Obtiene Datos de Clientes desde el Backend

--- src/app/clients/client.service.ts
+++ src/app/clients/client.service.ts
@@ -1,17 +1,24 @@
 import {Injectable} from '@angular/core';
 import {Client} from "./client";
-import {CLIENT_LIST} from "./clients.json";
-import {Observable, of} from "rxjs";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() {
+  private urlEndPoint: string = 'http://localhost:8080/api/clients/list';
+
+  constructor(private http: HttpClient) {
   }
 
   getClients(): Observable<Client[]> {
-    return of(CLIENT_LIST);
+    //  Option 1
+    return this.http.get<Client[]>(this.urlEndPoint);
+
+    // Option 2
+    // import {Observable, of, map} from "rxjs";
+    // return this.http.get(this.urlEndPoint).pipe(map(response => response as Client[]));
   }
 }
--- src/app/clients/client.ts
+++ src/app/clients/client.ts
@@ -5,14 +5,16 @@ export class Client {
   email: string = '';
   createdAt: string = '';
   updatedAt: string = '';
+  image: string = '';
 
 
-  constructor(id: number, name: string, surname: string, email: string, createdAt: string, updatedAt: string) {
+  constructor(id: number, name: string, surname: string, email: string, createdAt: string, updatedAt: string, image: string) {
     this.id = id;
     this.name = name;
     this.surname = surname;
     this.email = email;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
+    this.image = image;
   }
 }
