0470ffcd6c4a3717fe9f58d0e3b93897152bbc42
Luis B <luis.beroiza@adesso.de>
2023-08-30 13:59:11 +0200

    Inicializa la lista con objetos del tipo Cliente

--- src/app/clients/clients.json.ts
+++ src/app/clients/clients.json.ts
@@ -1,12 +1,12 @@
 import {Client} from "./client";
 
 export const CLIENT_LIST: Client[] = [
-  {id: 1, name: 'Luis', surname: 'Beroiza', email: 'luis@beroiza.com', createdAt: '1983-10-06', updatedAt: '2023-02-02', image: ''},
-  {id: 2, name: 'user2', surname: 'surname2', email: 'user2@email.com', createdAt: '1990-11-26', updatedAt: '2023-02-02', image: ''},
-  {id: 3, name: 'user3', surname: 'surname3', email: 'user3@email.com', createdAt: '1990-11-26', updatedAt: '2023-02-02', image: ''},
-  {id: 4, name: 'user4', surname: 'surname4', email: 'user4@email.com', createdAt: '1990-11-26', updatedAt: '2023-02-02', image: ''},
-  {id: 5, name: 'user5', surname: 'surname5', email: 'user5@email.com', createdAt: '1990-11-26', updatedAt: '2023-02-02', image: ''},
-  {id: 6, name: 'user6', surname: 'surname6', email: 'user6@email.com', createdAt: '1990-11-26', updatedAt: '2023-02-02', image: ''},
-  {id: 7, name: 'user7', surname: 'surname7', email: 'user7@email.com', createdAt: '1990-11-26', updatedAt: '2023-02-02', image: ''},
-  {id: 8, name: 'user8', surname: 'surname8', email: 'user8@email.com', createdAt: '1990-11-26', updatedAt: '2023-02-02', image: ''}
+  new Client(1, 'Luis', 'Beroiza', 'luis@beroiza.com', '1983-10-06', '2023-02-02', ''),
+  new Client(2, 'user2', 'surname2', 'user2@email.com', '1990-11-26', '2023-02-02', ''),
+  new Client(3, 'user3', 'surname3', 'user3@email.com', '1990-11-26', '2023-02-02', ''),
+  new Client(4, 'user4', 'surname4', 'user4@email.com', '1990-11-26', '2023-02-02', ''),
+  new Client(5, 'user5', 'surname5', 'user5@email.com', '1990-11-26', '2023-02-02', ''),
+  new Client(6, 'user6', 'surname6', 'user6@email.com', '1990-11-26', '2023-02-02', ''),
+  new Client(7, 'user7', 'surname7', 'user7@email.com', '1990-11-26', '2023-02-02', ''),
+  new Client(8, 'user8', 'surname8', 'user8@email.com', '1990-11-26', '2023-02-02', '')
 ];
