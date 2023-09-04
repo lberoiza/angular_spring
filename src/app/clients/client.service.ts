7987223f7564dbe5fbb985ec521a464995584928
Luis B <luis.beroiza@adesso.de>
2023-09-04 11:32:53 +0200

    Agrega Manejo de errores en el Frontend

--- src/app/clients/client.service.ts
+++ src/app/clients/client.service.ts
@@ -1,8 +1,13 @@
 import {Injectable} from '@angular/core';
 import {Client} from "./client";
-import {Observable, map} from "rxjs";
+import {Observable, map, catchError, throwError} from "rxjs";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {AlertMessage, ApiResponseTyp} from "../Types";
+import ApiResponse from "../Utils/ApiResponse";
+import {Router} from "@angular/router";
+import {AlertService} from "../services/alert.service";
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,35 +18,59 @@ export class ClientService {
   private urlEndPointClient = environment.apiUrl + '/clients';
   private urlEndPointList: string = this.urlEndPointClient + '/list';
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private alert: AlertService) {
   }
 
   public getClients(): Observable<Client[]> {
-    return this.http.get<Client[]>(this.urlEndPointList).pipe(
-      map(responseList => responseList.map(response => Client.of(response)
+    return this.http.get<ApiResponseTyp<Client[]>>(this.urlEndPointList)
+      .pipe(map(apiResponse => apiResponse.response.map(response => Client.of(response)
     )));
   }
 
   public createClient(client: Client): Observable<Client> {
     return this.http
-      .post<Client>(this.urlEndPointClient, client, {headers: this.httpHeaders})
-      .pipe(map(responseClient => Client.of(responseClient)));
+      .post<ApiResponseTyp<Client>>(this.urlEndPointClient, client, {headers: this.httpHeaders})
+      .pipe<ApiResponseTyp<Client>, Client>(
+        catchError(e => this.errorHandler(e)),
+        map(apiResponse => Client.of(apiResponse.response)));
   }
 
   public getClient(id: number): Observable<Client> {
     return this.http
-      .get<Client>(`${this.urlEndPointClient}/${id}`)
-      .pipe(map(responseClient => Client.of(responseClient)));
+      .get<ApiResponseTyp<Client>>(`${this.urlEndPointClient}/${id}`)
+      .pipe<ApiResponseTyp<Client>, Client>(
+        catchError(e => this.errorHandler(e, '/clients')),
+        map(apiResponse => Client.of(apiResponse.response)));
   }
 
   public updateClient(client: Client): Observable<Client> {
     return this.http
-      .put<Client>(`${this.urlEndPointClient}/${client.id}`, client, {headers: this.httpHeaders})
-      .pipe(map(responseClient => Client.of(responseClient)));
+      .put<ApiResponseTyp<Client>>(`${this.urlEndPointClient}/${client.id}`, client, {headers: this.httpHeaders})
+      .pipe<ApiResponseTyp<Client>, Client>(
+        catchError(e => this.errorHandler(e)),
+        map(apiResponse => Client.of(apiResponse.response)));
   }
 
   public deleteClient(client: Client): Observable<Client> {
     return this.http
-      .delete<Client>(`${this.urlEndPointClient}/${client.id}`, {headers: this.httpHeaders});
+      .delete<Client>(`${this.urlEndPointClient}/${client.id}`, {headers: this.httpHeaders})
+      .pipe(catchError(e => this.errorHandler(e)));
   }
+
+  private errorHandler(e: any, navigateTo: string = "") {
+    const apiResponse = new ApiResponse<Client>(e.error as ApiResponseTyp<Client>);
+    if(navigateTo.length > 0){
+      this.router.navigate([navigateTo]).then();
+    }
+    const alertMessage: AlertMessage = {
+      title: 'Error',
+      content: apiResponse.getErrorsMessages().join(',')
+    };
+    this.alert.showError(alertMessage);
+    return throwError(e);
+  }
+
 }
