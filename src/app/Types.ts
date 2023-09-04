e25cf4156d07c881d1331be4ac5954791bc97362
Luis B <luis.beroiza@adesso.de>
2023-09-04 11:30:17 +0200

    Agrega Tipo de Respuesta Backend

--- src/app/Types.ts
+++ src/app/Types.ts
@@ -7,3 +7,9 @@ export type AlertMessage = {
   title: string,
   content: string
 };
+
+export type ApiResponseTyp<T> = {
+  messages: {[key: string]: string[]},
+  isSuccessfully: boolean,
+  response: T
+};
