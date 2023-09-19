cd531fca2cb3ab78f0794f1ce959bfae4f154c8a
Luis B <luis.beroiza@hotmail.com>
2023-09-19 22:00:36 +0200

    Commit para probar el cambio de autor

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -43,7 +43,6 @@ export class ClientsComponent implements OnInit {
     });
   }
 
-
   deleteClient(clientToDelete: Client): void {
     const message: AlertMessage = {
       title: 'Delete Client',
